ee4437f05251178109b1b2a50da9f7869471b585
Israel Rabi <israelrabi01@gmail.com>
2024-05-10 07:46:45 -0300

    2023 day1 part 1 solved

---
+++ 2023/python/day1-1.py
@@ -0,0 +1,14 @@
+import sys
+
+
+def get_numbers(line):
+    return [num for num in line if num.isnumeric()]
+
+
+if __name__ == "__main__":
+    the_sum = 0
+    with open(sys.argv[1]) as file:
+        for line in file:
+            nums = get_numbers(line.strip())
+            the_sum += int(str(nums[0]) + str(nums[-1]))
+    print(the_sum)
---
+++ 2023/python/day1-2.py
@@ -0,0 +1,6 @@
+dicio = {"zero": 4, "one": 3, "two": 3, "three": 5,
+         "four": 4, "five": 4, "six": 3,
+         "seven": 5, "eight": 5, "nine": 4}
+
+if __name__ == "__main__":
+    pass
